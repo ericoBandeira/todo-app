fb6540dedd213a6a2a2a357034856b065addc9c1
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2022-06-06 23:56:36 -0300

    Finish todo app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map(task => ({ ...task }))
+    
+    const foundTask = updateTasks.find(item => item.id === id)
+
+    if (!foundTask)
+      return
+    
+    foundTask.done = !foundTask.done
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
